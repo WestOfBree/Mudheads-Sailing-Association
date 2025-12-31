a0963206ce47245cbac891e7cd1320cd823b6187
Brianna Thomas <bthomas4@my.ccri.edu>
2025-12-31 13:03:30 -0500

    page title changed

--- src/Pages/Membership.jsx
+++ src/Pages/Membership.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import memberForm from "../assets/membershipformMH-2026.pdf";
 import { Form } from "react-router-dom";
 
-
 function Membership() {
   return (
     <div className="row Membership__row">
@@ -31,76 +30,116 @@ function Membership() {
           </h5>
         </div>
         <div className="Membership__form--container">
-            <form className="membership__form">
-              <label>
-                Your Name:
-                <input type="text" name="name" required />
-              </label>
-              <label>
-                Your Email:
-                <input type="email" name="email" required />
-              </label>
-              <label>
-                Membership Level:
-                <select name="membershipLevel" required>
-                  <option value="junior">Junior (18 and younger)</option>
-                  <option value="adult">Adult (19 and older)</option>
-                </select>
-              </label>
-              <label>
-                Address:
-                <input type="text" name="address" />
-              </label>
-              <label>
-                City:
-                <input type="text" name="city"  />
-              </label>
-              <label>
-                State:
-                <input type="text" name="state"  />
-              </label>
-              <label>
-                Zip Code:
-                <input type="text" name="zip"  />
-              </label>
-              <label>
-                Your Phone Number:
-                <input type="tel" name="phone"  />
-              </label>
-              <label>
-                Boat Affiliation (Name, Type, and Relationship to you):
-                <input type="text" name="boatAffiliation"  />
-              </label>
-              <label>
-                Would you like to volunteer for events?
-                </label>
-                <select name="volunteer" required>
-                  <option value="yes">Yes! Sign me up!</option>
-                  <option value="no">No thank you, not at this time.</option>
-                </select>
-                <label>
-                    Check all that apply:  
-                    <br />
-                    <input type="checkbox" name="volunteerOptions" value="raceCommittee" /> RC Boat Commissioning Crew (April/May)
-                    <input type="checkbox" name="volunteerOptions" value="raceCommittee" /> Race Committee (Weds. Nights - May/September)
-                    <input type="checkbox" name="volunteerOptions" value="raceCommittee" /> Mid-Season Party (July)
-                    <input type="checkbox" name="volunteerOptions" value="raceCommittee" /> Mudhead Benefit Cup Regatta (July)
-                    <input type="checkbox" name="volunteerOptions" value="raceCommittee" /> Awards Banquet (September)
-                    <input type="checkbox" name="volunteerOptions" value="raceCommittee" /> Annual Meeting & Holiday Party (December)
-                    <input type="checkbox" name="volunteerOptions" value="generalSupport" /> General Support (Year 'Round)
+          <form className="membership__form">
+            <label>
+              Your Name:
+              <input type="text" name="name" required />
             </label>
-              <label>
-                Ideas/Remarks/Juicy Tidbits for our Web Site?
-                <textarea name="comments" rows="4" />
-              </label>
-              <button type="submit">Submit</button>
-            </form>
+            <label>
+              Your Email:
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              Membership Level:
+              <select name="membershipLevel" required>
+                <option value="junior">Junior (18 and younger)</option>
+                <option value="adult">Adult (19 and older)</option>
+              </select>
+            </label>
+            <label>
+              Address:
+              <input type="text" name="address" />
+            </label>
+            <label>
+              City:
+              <input type="text" name="city" />
+            </label>
+            <label>
+              State:
+              <input type="text" name="state" />
+            </label>
+            <label>
+              Zip Code:
+              <input type="text" name="zip" />
+            </label>
+            <label>
+              Your Phone Number:
+              <input type="tel" name="phone" />
+            </label>
+            <label>
+              Boat Affiliation (Name, Type, and Relationship to you):
+              <input type="text" name="boatAffiliation" />
+            </label>
+            <label>Would you like to volunteer for events?</label>
+            <select name="volunteer" required>
+              <option value="yes">Yes! Sign me up!</option>
+              <option value="no">No thank you, not at this time.</option>
+            </select>
+            <label>
+              Check all that apply:
+              <br />
+              <input
+                type="checkbox"
+                name="volunteerOptions"
+                value="raceCommittee"
+              />{" "}
+              RC Boat Commissioning Crew (April/May)
+              <input
+                type="checkbox"
+                name="volunteerOptions"
+                value="raceCommittee"
+              />{" "}
+              Race Committee (Weds. Nights - May/September)
+              <input
+                type="checkbox"
+                name="volunteerOptions"
+                value="raceCommittee"
+              />{" "}
+              Mid-Season Party (July)
+              <input
+                type="checkbox"
+                name="volunteerOptions"
+                value="raceCommittee"
+              />{" "}
+              Mudhead Benefit Cup Regatta (July)
+              <input
+                type="checkbox"
+                name="volunteerOptions"
+                value="raceCommittee"
+              />{" "}
+              Awards Banquet (September)
+              <input
+                type="checkbox"
+                name="volunteerOptions"
+                value="raceCommittee"
+              />{" "}
+              Annual Meeting & Holiday Party (December)
+              <input
+                type="checkbox"
+                name="volunteerOptions"
+                value="generalSupport"
+              />{" "}
+              General Support (Year 'Round)
+            </label>
+            <label>
+              Ideas/Remarks/Juicy Tidbits for our Web Site?
+              <textarea name="comments" rows="4" />
+            </label>
+            <button
+              onClick={() =>
+                alert(
+                  "Thank you for your interest, however this page is still underconstruction and the submit form button does not work yet. Please contact mudheads.org for assistance"
+                )
+              }
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-    </div>
   );
-
 }
-
 
 export default Membership;
